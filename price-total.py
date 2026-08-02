0c144ad52099ea8a9fc04adaacffbdca72b9544f
Andrey Sarantsev <50476640+asarantsev@users.noreply.github.com>
2026-08-02 13:56:55 -0700

    Add files via upload

--- price-total.py
+++ price-total.py
@@ -6,30 +6,39 @@ from matplotlib import pyplot as plt
 from statsmodels.graphics.tsaplots import plot_acf
 from scipy.stats import shapiro
 
+# price = arithmetic price returns in %
+# total = arithmetic total returns in %
 def compute(price, total, key):
     print('')
     print(key)
     print('')
-    ltotal = np.log(1 + total * 0.01)
-    lwealth = np.append(np.array([0]), np.cumsum(ltotal))
+    ltotalret = np.log(1 + total * 0.01)
+    lpriceret = np.log(1 + price * 0.01)
+    lwealth = np.append(np.array([0]), np.cumsum(ltotalret))
+    lindex = np.append(np.array([0]), np.cumsum(lpriceret))
     wealth = np.exp(lwealth)
-    dpaid = 0.01 * (total - price) * wealth[:-1]
-    premeasure = lwealth[1:] - np.log(dpaid)
-    Reg = OLS(np.diff(premeasure), pd.DataFrame({'const' : 1, 'lag' : premeasure[:-1], 'trend': range(len(premeasure) - 1)})).fit()
-    print(Reg.summary())
-    res = Reg.resid
-    plot_acf(res)
-    plt.title('original residuals ' + key)
-    plt.show()
-    plot_acf(abs(res))
-    plt.title('absolute residuals ' + key)
-    plt.show()
-    print('Shapiro-Wilk p = ', shapiro(res)[1])
-    print('Trend = ', -Reg.params['trend']/Reg.params['lag'])
+    index = np.exp(lindex)
+    dpaid = 0.01 * (total - price) * index[:-1]
+    if min(dpaid) < 0:
+        return -1
+    if min(dpaid) > 0:
+        premeasure = lwealth[1:] - np.log(dpaid)
+        Reg = OLS(np.diff(premeasure), pd.DataFrame({'const' : 1, 'lag' : premeasure[:-1], 'trend': range(len(premeasure) - 1)})).fit()
+        print(Reg.summary())
+        res = Reg.resid
+        plot_acf(res)
+        plt.title('original residuals ' + key)
+        plt.show()
+        plot_acf(abs(res))
+        plt.title('absolute residuals ' + key)
+        plt.show()
+        print('Shapiro-Wilk p = ', shapiro(res)[1])
+        print('Trend = ', -Reg.params['trend']/Reg.params['lag'])
+        return 0
     
 priceDF = pd.read_excel('price-total.xlsx', sheet_name = 'price')
 totalDF = pd.read_excel('price-total.xlsx', sheet_name = 'total')
-keys = ['Lo 30', 'Med 40', 'Hi 30', 'Lo 20', 'Qnt 2', 'Qnt 3', 'Qnt 4', 'Hi 20', '2-Dec', '3-Dec', '4-Dec', '5-Dec', '6-Dec', '7-Dec', '8-Dec', '9-Dec', 'Hi 10']
+keys = ['Lo 30', 'Med 40', 'Hi 30', 'Lo 20', 'Qnt 2', 'Qnt 3', 'Qnt 4', 'Hi 20', 'Lo 10', '2-Dec', '3-Dec', '4-Dec', '5-Dec', '6-Dec', '7-Dec', '8-Dec', '9-Dec', 'Hi 10']
 for key in keys:
     price = priceDF[key].values
     total = totalDF[key].values
