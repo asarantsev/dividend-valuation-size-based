49cebc7c873af48bf82ec7b996d20e3c94606eda
Andrey Sarantsev <50476640+asarantsev@users.noreply.github.com>
2026-08-02 18:16:43 -0700

    Add files via upload

--- price-total.py
+++ price-total.py
@@ -4,14 +4,29 @@ from matplotlib import pyplot as plt
 from statsmodels.api import OLS
 from matplotlib import pyplot as plt
 from statsmodels.graphics.tsaplots import plot_acf
-from scipy.stats import shapiro
+from scipy import stats 
+from statsmodels.stats.diagnostic import acorr_ljungbox as LB
 
-# price = arithmetic price returns in %
-# total = arithmetic total returns in %
+# analysis of residuals 
+def check(res, key):
+    print(LB(res, lags=[5], return_df = True)['lb_pvalue'].values[0])
+    print(LB(abs(res), lags = [5], return_df = True)['lb_pvalue'].values[0])
+    plot_acf(res, zero = False)
+    plt.title('Original Residuals ' + key)
+    plt.savefig(key + '-original.png')
+    plt.close()
+    plot_acf(abs(res), zero = False)
+    plt.title('Absolute Residuals ' + key)
+    plt.savefig(key + '-absolute.png')
+    plt.close()
+    print('Shapiro-Wilk p = ', stats.shapiro(res)[1])
+
+# price = arithmetic price returns in %, total = arithmetic total returns in %
 def compute(price, total, key):
     print('')
     print(key)
     print('')
+    N = len(price)
     ltotalret = np.log(1 + total * 0.01)
     lpriceret = np.log(1 + price * 0.01)
     lwealth = np.append(np.array([0]), np.cumsum(ltotalret))
@@ -23,17 +38,17 @@ def compute(price, total, key):
         return -1
     if min(dpaid) > 0:
         premeasure = lwealth[1:] - np.log(dpaid)
-        Reg = OLS(np.diff(premeasure), pd.DataFrame({'const' : 1, 'lag' : premeasure[:-1], 'trend': range(len(premeasure) - 1)})).fit()
+        Reg = OLS(np.diff(premeasure), pd.DataFrame({'const' : 1, 'lag' : premeasure[:-1], 'trend': range(N - 1)})).fit()
         print(Reg.summary())
         res = Reg.resid
-        plot_acf(res)
-        plt.title('original residuals ' + key)
-        plt.show()
-        plot_acf(abs(res))
-        plt.title('absolute residuals ' + key)
-        plt.show()
-        print('Shapiro-Wilk p = ', shapiro(res)[1])
-        print('Trend = ', -Reg.params['trend']/Reg.params['lag'])
+        check(res, key + '-measure')
+        trend = -Reg.params['trend']/Reg.params['lag']
+        measure = premeasure - trend * range(N)
+        plt.plot(measure)
+        plt.title('Valuation Measure For ' + key)
+        plt.savefig(key + '-valuation.png')
+        plt.close()
+        print('Trend = ', trend)
         return 0
     
 priceDF = pd.read_excel('price-total.xlsx', sheet_name = 'price')
@@ -42,4 +57,4 @@ keys = ['Lo 30', 'Med 40', 'Hi 30', 'Lo 20', 'Qnt 2', 'Qnt 3', 'Qnt 4', 'Hi 20',
 for key in keys:
     price = priceDF[key].values
     total = totalDF[key].values
-    compute(price, total, key)
+    compute(price, total, key)        
